20993c91fb6355ae033178b2faa6ad2ca2b90bf9
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2025-04-01 19:14:40 -0300

    executando o push para limpar o ambiente

--- APIRedeSo/APIRedeSo/obj/Release/net8.0/APIRedeSo.AssemblyInfo.cs
+++ APIRedeSo/APIRedeSo/obj/Release/net8.0/APIRedeSo.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIRedeSo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d67ea3c3e37682383e58ddc940b10f9d7b757fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIRedeSo")]
